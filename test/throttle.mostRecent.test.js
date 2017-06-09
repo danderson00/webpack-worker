b3ccf1769fb4b1227ec2454067c8638f1cb9435a
danderson00 <danderson00@gmail.com>
2017-06-08 19:45:56 -0700

    Add most recent throttle. Add launch.json

---
+++ test/throttle.mostRecent.test.js
@@ -0,0 +1,75 @@
+var throttle = require('../throttle.mostRecent')
+
+test("queues executions", done => {
+  let o = generateOperation()
+  o.operation(1)
+  o.operation(2)
+  expect(o.operations.map(x => x.resolved)).toEqual([false])
+  o.operations[0].trigger()
+  // not sure why this doesn't happen synchronously...
+  setTimeout(() => {
+    expect(o.operations.map(x => x.resolved)).toEqual([true, false])
+    o.operations[1].trigger()
+    setTimeout(() => {
+      expect(o.operations.map(x => x.resolved)).toEqual([true, true])
+      expect(o.operations.map(x => x.args)).toEqual([[1], [2]])
+      done()
+    })
+  })
+})
+
+test("rejects intermediate executions", done => {
+  let rejected = false
+  let o = generateOperation()
+  o.operation(1, 2)
+  o.operation(2, 3).catch(() => rejected = true)
+  o.operation(3, 4)
+  expect(o.operations.map(x => x.resolved)).toEqual([false])
+  o.operations[0].trigger()
+  // not sure why this doesn't happen synchronously...
+  setTimeout(() => {
+    expect(o.operations.map(x => x.resolved)).toEqual([true, false])
+    o.operations[1].trigger()
+    setTimeout(() => {
+      expect(o.operations.map(x => x.resolved)).toEqual([true, true])
+      expect(o.operations.map(x => x.args)).toEqual([[1, 2], [3, 4]])
+      expect(rejected).toBe(true)
+      done()
+    })
+  })
+})
+
+test("rejecting promise executes next operation", done => {
+  let o = generateOperation()
+  o.operation(1)
+  o.operation(2)
+  expect(o.operations.map(x => x.resolved)).toEqual([false])
+  o.operations[0].fail()
+  // not sure why this doesn't happen synchronously...
+  setTimeout(() => {
+    expect(o.operations.map(x => x.resolved)).toEqual([false, false])
+    o.operations[1].trigger()
+    setTimeout(() => {
+      expect(o.operations.map(x => x.resolved)).toEqual([false, true])
+      expect(o.operations.map(x => x.args)).toEqual([[1], [2]])
+      done()
+    })
+  })
+})
+
+function generateOperation() {
+  let result = { operations: [] }
+  result.operation = throttle(function() {
+    var operationResult = { resolved: false, args: Array.from(arguments) }
+    result.operations.push(operationResult)
+    operationResult.promise = new Promise((resolve, reject) => {
+      operationResult.trigger = () => {
+        resolve()
+        operationResult.resolved = true
+      }
+      operationResult.fail = () => reject()
+    })
+    return operationResult.promise
+  })
+  return result
+}
---
+++ throttle.mostRecent.js
@@ -0,0 +1,38 @@
+// intended for fire and forget operations
+// if the function is executed while an existing operation in in progress, 
+// only the most recent subsequent execution will be triggered when the operation completes
+module.exports = function throttle(func) {
+  let current, next
+
+  const executeNext = () => {
+    if(next) {
+      current = next()
+      next = undefined
+      current.then(() => {
+        current = undefined
+        executeNext()
+      })
+    }
+  }
+
+  return function() {
+    var args = Array.from(arguments)
+    var result = new Promise((resolve, reject) => {
+      if(next)
+        next.drop()
+
+      next = () => Promise.resolve(func.apply(this, args))
+        .then(result => resolve(result))
+        .catch(error => reject(error))
+
+      next.drop = () => {
+        var error = new Error('The operation was dropped')
+        error.dropped = true
+        reject(error)
+      }
+
+      if(!current) executeNext()
+    })
+    return result;
+  }
+}
